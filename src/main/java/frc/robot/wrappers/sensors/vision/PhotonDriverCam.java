444885363ce70b9a11cfee7c50a2be92a205a2b2
MqxS <maxgoog06@outlook.com>
2023-03-21 17:32:13 -0400

    cleaning :)

--- src/main/java/frc/robot/wrappers/sensors/vision/PhotonDriverCam.java
+++ src/main/java/frc/robot/wrappers/sensors/vision/PhotonDriverCam.java
@@ -1,36 +1,10 @@
 package frc.robot.wrappers.sensors.vision;
 
-import edu.wpi.first.math.geometry.Pose2d;
 import org.photonvision.PhotonCamera;
-import org.photonvision.targeting.PhotonPipelineResult;
-import org.photonvision.targeting.PhotonTrackedTarget;
 
 public class PhotonDriverCam {
-    private final PhotonCamera photonCamera;
 
     public PhotonDriverCam(PhotonCamera photonCamera) {
-        this.photonCamera = photonCamera;
         photonCamera.setDriverMode(true);
-    }
-
-    public PhotonPipelineResult getLatestResult() {
-        return photonCamera.getLatestResult();
-    }
-
-    public Pose2d getRobotPoseRelativeToAprilTag(PhotonPipelineResult pipelineResult) {
-        if (hasTargets(pipelineResult)) {
-            PhotonTrackedTarget bestTarget = pipelineResult.getBestTarget();
-            // Single best target
-            return new Pose2d(bestTarget.getBestCameraToTarget().getTranslation().toTranslation2d(), bestTarget.getBestCameraToTarget().getRotation().toRotation2d());
-        }
-        return new Pose2d();
-    }
-
-    public boolean hasTargets(PhotonPipelineResult pipelineResult) {
-        return pipelineResult.hasTargets();
-    }
-
-    public int targetId(PhotonPipelineResult pipelineResult) {
-        return pipelineResult.hasTargets() ? pipelineResult.getBestTarget().getFiducialId() : -1;
     }
 }
