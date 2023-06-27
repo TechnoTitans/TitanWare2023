00623e1f4a27d01359672f6db9129b6906c602a9
MqxS <maxgoog06@outlook.com>
2023-06-27 12:28:06 -0400

    Can be replaced with pose2d.log();

--- src/main/java/frc/robot/utils/PoseUtils.java
+++ src/main/java/frc/robot/utils/PoseUtils.java
@@ -7,6 +7,7 @@ import edu.wpi.first.math.geometry.Twist2d;
 import edu.wpi.first.wpilibj.DriverStation;
 import frc.robot.Constants;
 
+@SuppressWarnings("unused")
 public class PoseUtils {
     public static boolean poseWithinArea(
             final Pose2d currentPose,
@@ -48,26 +49,5 @@ public class PoseUtils {
                     originalPose.getY(),
                     originalPose.getRotation().times(-1))
                 : originalPose;
-    }
-
-    //I stole this from 254
-    // what in the world is this bro
-    @SuppressWarnings("unused")
-    public static Twist2d PoseLog(final Pose2d transform) {
-        final double kEps = 1E-9;
-        final double dtheta = transform.getRotation().getRadians();
-        final double half_dtheta = 0.5 * dtheta;
-        final double cos_minus_one = transform.getRotation().getCos() - 1.0;
-        final double halftheta_by_tan_of_halfdtheta;
-
-        if (Math.abs(cos_minus_one) < kEps) {
-            halftheta_by_tan_of_halfdtheta = 1.0 - 1.0 / 12.0 * dtheta * dtheta;
-        } else {
-            halftheta_by_tan_of_halfdtheta = -(half_dtheta * transform.getRotation().getSin()) / cos_minus_one;
-        }
-        final Translation2d translation_part = transform.getTranslation().rotateBy(
-                new Rotation2d(halftheta_by_tan_of_halfdtheta, -half_dtheta));
-
-        return new Twist2d(translation_part.getX(), translation_part.getY(), dtheta);
     }
 }
