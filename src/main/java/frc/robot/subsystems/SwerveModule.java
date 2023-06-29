189aefde16dbc54ef05cfe24f37c6c36f3284569
Atman Singh <39840838+Atman-Singh@users.noreply.github.com>
2023-06-29 13:31:55 -0400

    Fixed it ice skating

--- src/main/java/frc/robot/subsystems/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -4,7 +4,7 @@ import com.ctre.phoenix6.BaseStatusSignal;
 import com.ctre.phoenix6.configs.CANcoderConfiguration;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.PositionVoltage;
-import com.ctre.phoenix6.controls.VelocityTorqueCurrentFOC;
+import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.FeedbackSensorSourceValue;
@@ -20,8 +20,9 @@ public class SwerveModule {
     private final CANcoder turnEncoder;
     private final double magnetOffset;
     private final InvertedValue driveInvertedValue, turnInvertedValue;
-    private final VelocityTorqueCurrentFOC torqueVelocity;
-    private final PositionVoltage positionVelocity;
+
+    private final VelocityVoltage velocityVoltage;
+    private final PositionVoltage positionVoltage;
 
     public SwerveModule(
             final TalonFX driveMotor,
@@ -38,8 +39,8 @@ public class SwerveModule {
         this.driveInvertedValue = driveInvertedValue;
         this.turnInvertedValue = turnInvertedValue;
 
-        this.torqueVelocity = new VelocityTorqueCurrentFOC(0);
-        this.positionVelocity = new PositionVoltage(0);
+        this.velocityVoltage = new VelocityVoltage(0);
+        this.positionVoltage = new PositionVoltage(0);
 
         config();
     }
@@ -51,9 +52,9 @@ public class SwerveModule {
 
         final TalonFXConfiguration driverConfig = new TalonFXConfiguration();
         driverConfig.Slot0 = Constants.Modules.DRIVE_MOTOR_CONSTANTS;
-        driverConfig.TorqueCurrent.PeakForwardTorqueCurrent = 60;
-        driverConfig.TorqueCurrent.PeakReverseTorqueCurrent = -60;
-        driverConfig.ClosedLoopRamps.TorqueClosedLoopRampPeriod = 0.2;
+        driverConfig.CurrentLimits.StatorCurrentLimit = 60;
+        driverConfig.CurrentLimits.StatorCurrentLimitEnable = true;
+        driverConfig.ClosedLoopRamps.VoltageClosedLoopRampPeriod = 0.2;
         driverConfig.Feedback.SensorToMechanismRatio = Constants.Modules.DRIVER_GEAR_RATIO;
         driverConfig.MotorOutput.NeutralMode = NeutralModeValue.Coast;
         driverConfig.MotorOutput.Inverted = driveInvertedValue;
@@ -61,8 +62,8 @@ public class SwerveModule {
 
         final TalonFXConfiguration turnerConfig = new TalonFXConfiguration();
         turnerConfig.Slot0 = Constants.Modules.TURN_MOTOR_CONSTANTS;
-//        turnerConfig.Voltage.PeakForwardVoltage = 6;
-//        turnerConfig.Voltage.PeakReverseVoltage = -6;
+        turnerConfig.Voltage.PeakForwardVoltage = 6;
+        turnerConfig.Voltage.PeakReverseVoltage = -6;
         turnerConfig.Feedback.FeedbackRemoteSensorID = turnEncoder.getDeviceID();
         turnerConfig.Feedback.FeedbackSensorSource = FeedbackSensorSourceValue.FusedCANcoder;
         turnerConfig.Feedback.RotorToSensorRatio = Constants.Modules.TURNER_GEAR_RATIO;
@@ -112,8 +113,8 @@ public class SwerveModule {
         final double desired_driver_velocity = wantedState.speedMetersPerSecond / Constants.Modules.WHEEL_CIRCUMFERENCE;
         final double desired_turner_rotations = wantedState.angle.getRotations();
 
-        driveMotor.setControl(torqueVelocity.withVelocity(desired_driver_velocity));
-        turnMotor.setControl(positionVelocity.withPosition(desired_turner_rotations));
+        driveMotor.setControl(velocityVoltage.withVelocity(desired_driver_velocity));
+        turnMotor.setControl(positionVoltage.withPosition(desired_turner_rotations));
     }
 
     public void stop() {
