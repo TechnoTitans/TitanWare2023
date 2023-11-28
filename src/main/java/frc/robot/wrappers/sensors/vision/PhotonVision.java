af271994f293d09d5a552918dde1a46d83c727f7
harry.chen <harryxtchen@gmail.com>
2023-11-28 10:08:48 -0500

    No need to use gyro only for vision updates, this is a temporary fix leftover from GRITS

--- src/main/java/frc/robot/wrappers/sensors/vision/PhotonVision.java
+++ src/main/java/frc/robot/wrappers/sensors/vision/PhotonVision.java
@@ -241,14 +241,15 @@ public class PhotonVision<T extends PhotonVisionIO> extends VirtualSubsystem {
 
             lastStableEstimatedPosesByCamera.put(photonVisionIO, stableEstimatedRobotPose);
 
-            final Pose2d estimatedPoseWithGyro = new Pose2d(
-                    estimatedRobotPose.estimatedPose.getTranslation().toTranslation2d(),
-                    swerve.getYaw()
-            );
+//            final Pose2d estimatedPoseWithGyro = new Pose2d(
+//                    estimatedRobotPose.estimatedPose.getTranslation().toTranslation2d(),
+//                    swerve.getYaw()
+//            );
 
             // TODO: get better calibrations on cameras or get better cameras
             poseEstimator.addVisionMeasurement(
-                    estimatedPoseWithGyro,
+//                    estimatedPoseWithGyro,
+                    estimatedRobotPose.estimatedPose.toPose2d(),
                     estimatedRobotPose.timestampSeconds
             );
         }
